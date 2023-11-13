8ecef25ec57cdc15f04c78e8be27e81fa6ed4db5
Simon_Paul <simonchasnovsky@gmail.com>
2023-11-13 16:36:14 -0300

    Finish bookmark functionality

--- index.js
+++ index.js
@@ -11,128 +11,144 @@ async function doTreeStuff() {
     const bookmarkTree = (bookmarkObj[0]);
 
     // Function to recursively iterate through the bookmark array to sort it
-    function recurse(folder) {
-        folder.children.forEach(function(subItem, i) {
-            if (subItem.children) {
-                menu.push('SUBFOLDER ' + subItem.title);
+    function recurseSort(subFolder, parent) {
+        subFolder.forEach(function(subItem, i) {
 
-                recurse(subItem);
+            if (subItem.children) {
+                let subFolderObj = {name: subItem.title, subitems: []};
+                
+                parent.subitems.push(subFolderObj);
+
+                let subFolder = subItem.children;
+
+                recurseSort(subFolder, subFolderObj);
             } else {
-                if (i == folder.children.length - 1) {
-                    menu.push('LAST-FILE ' + subItem.title);
-                } else {
-                    menu.push('FILE ' + subItem.title);
-                }
+
+                let file = {name: subItem.title, link: subItem.url};
+
+                parent.subitems.push(file);
+            }
+        });
+    }
+
+    // Function to recursively iterate through the bookmark menu render it
+    function recurseRender(element, parent) {
+        element.subitems.forEach(function(subElement) {
+            if (subElement.subitems) {
+                const bookmarkSubMenu = document.createElement('li');
+                bookmarkSubMenu.setAttribute("class", "subfolder inactive");
+        
+                bookmarkSubMenu.innerHTML = `
+                <a href="#" class="subfolder-title">
+                    <span class="subfolder-icon">🗀</span>
+                    <span class="subfolder-name">${subElement.name}</span>
+                </a>`;
+
+                const subSubList = document.createElement('ul');
+                subSubList.setAttribute('class', 'subroot inactive')
+        
+                bookmarkSubMenu.appendChild(subSubList);
+
+                parent.appendChild(bookmarkSubMenu);
+
+                recurseRender(subElement, subSubList);
+            } else {
+                const bookmarkLink = document.createElement('li');
+                bookmarkLink.setAttribute("class", "file inactive");
+        
+                bookmarkLink.innerHTML = `
+                <a target="_blank" href="${subElement.link}" class="file-title">
+                    <span class="file-icon"></span>
+                    <span class="file-name">${subElement.name}</span>
+                </a>`;
+
+                parent.appendChild(bookmarkLink);
             }
         });
     }
 
     // Push bookmarks to the menu array and recurse to sort
     bookmarkTree.forEach(function(folder, i) {
-        menu.push('FOLDER ' + folder.title);
-        
-        recurse(folder);
+        let folderObj = {name: folder.title, subitems: []};
+
+        menu.push(folderObj);
+
+        let subFolder = folder.children;
+
+        recurseSort(subFolder, folderObj)
     });
 
-    // Separate bookmarks array at files containing the string "LAST"
-    const
-    separator = 'LAST-FILE',
-    groups = menu.reduce((r, s, i, a) => {
-        if (!i || a[i - 1].includes(separator)) r.push([]);
-        r[r.length - 1].push(s);
-        return r;
-    }, []);
-
-    // Regroup the bookmark array in its original folder structure
-    let grouped = groups.reduce((a, b, index) =>
-        !index || !/file|sub|last-file/i.test(b[0]) ? [...a, b] : (a[a.length -1].push(...b), a) , []);
-
-    
-    // Loop that renders the bookmark tree as folders by appending a template literal amount of HTML to the DOM
+    // Loop that renders the bookmark tree as nested unordered lists
     const bookmarkList = document.querySelector(".bookmark-list");
 
-    grouped.forEach(function(element, i) {
-
+    menu.forEach(function(element, i) {
         const bookmarkMenu = document.createElement('li');
-        bookmarkMenu.setAttribute("class", "folder-" + i);
-
-        const re = /(file|folder|subfolder|last-file)/i;
+        bookmarkMenu.setAttribute("class", "folder");
 
         bookmarkMenu.innerHTML = `
         <a href="#" class="folder-title">
-            <span class="file-icon">🗀</span>
-            <span class="file-name">${element[0].replace(re, '')}</span>
-        </a>
-        <ul class="root root-${i}">
-        ${element.map(subElement => subElement.includes('SUBFOLDER') ? 
-            `<ul class="subfolder">
-                <a href="#" class="folder-title">
-                    <span class="file-icon">🗀</span>
-                    <span class="file-name">${subElement.replace(re,'')}</span>
-                </a>
-            ` :
-        subElement.includes('FILE') ?
-            `<li class="${subElement}">${subElement.replace(re, '')}</li>` : ''
-        ).join('')}</ul>
-        `;
+            <span class="folder-icon">🗀</span>
+            <span class="folder-name">${element.name}</span>
+        </a>`;
+
+        const subList = document.createElement('ul');
+        subList.setAttribute('class', 'root inactive')
+
+        bookmarkMenu.appendChild(subList);
 
         bookmarkList.appendChild(bookmarkMenu);
+
+        recurseRender(element, subList);
     });
 
-    const lastElements = document.querySelectorAll(".LAST-FILE");
-    const repeatedEls = [];
-    const uniqueEls = [];
+    const masters = document.querySelectorAll(".folder-title");
 
-    // Get all last bookmarks and push the ones followed by <li> elements to an array
-    lastElements.forEach(function (lastElement, i) {
-        if (lastElement.nextElementSibling != null) {
-            const nextElement = lastElement.nextElementSibling;
-            if (nextElement.tagName == 'LI') {
-                repeatedEls.push(lastElement);
-            }
-        }
-    });
+    const files = document.getElementsByClassName("file");
+    const margins = document.getElementsByClassName("subfolder");
 
-    // Leave only one last bookmark per bookmark folder
-    for (i = 0; i < repeatedEls.length; i++) {
-        const current = repeatedEls[i];
-        if (i > 0) {
-            const previous = repeatedEls[i];
-            if (current.closest('.root') == previous.closest('.root')) {
-                uniqueEls.push(current);
-            }
-        }
+    // Apply margins to file structure view
+    for (let i = 0; i < margins.length; i++) {
+        margins[i].style.paddingLeft = '21px';
     }
+    for (let i = 0; i < files.length; i++) {
+        files[i].style.paddingLeft = '25px';
+    }
+    
+    // Open and close root folders
+    masters.forEach(function(master, i) {
+        master.addEventListener("click", function () {
+            const children = this.parentElement.childNodes[2];
+            const icon = this.childNodes[1];
 
-    // Select all <li> elements after the second to last <li> element with
-    // the 'LAST-FILE' class and move them to the root <ul>
-    for (i = 0; i < uniqueEls.length; i++) {
-        const lastElement = uniqueEls[i];
-        const root = lastElement.closest('.root');
-        const parent = lastElement.parentElement;
-        const siblings = parent.children;
-        let lastFiles = [];
+            children.classList.toggle("inactive");
 
-        for (const sibling of siblings) {
-            if (sibling.classList.contains('LAST-FILE')) {
-                lastFiles.push(sibling);
+            if (icon.innerHTML == "🗀") {
+                icon.innerHTML = "🗁";
+            } else {
+                icon.innerHTML = "🗀"
             }
-        }
+        });
+    });
 
-        const secondToLast = lastFiles[lastFiles.length - 2];
+    const folders = document.querySelectorAll(".subfolder-title");
 
-        const className = secondToLast.classList.value;
-        console.log(className);
+    // Open and close sub-folders
+    for (let i = 0; i < folders.length; i++) {
+        folders[i].addEventListener("click", function () {
+            const children = this.parentElement.childNodes[2];
+            const icon = this.childNodes[1];
 
-        const selector = '.' + className + ' ~ li';
+            children.classList.toggle("inactive");
 
-        // const toMove = document.querySelectorAll(selector);
-        // console.log(toMove);
+            if (icon.innerHTML == "🗀") {
+                icon.innerHTML = "🗁";
+            } else {
+                icon.innerHTML = "🗀"
+            }
+        });
     }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-
     doTreeStuff();
-
 });
